df58238a1a3aaf7f111a34e74feab0b85fd9c408
Divyansh Kachangal <dkachangal@gmail.com>
2025-11-24 01:23:32 +0530

    Task -2 Kafka Listner Class Crated

--- src/test/java/com/jpmc/midascore/TaskTwoTests.java
+++ src/test/java/com/jpmc/midascore/TaskTwoTests.java
@@ -20,6 +20,7 @@ class TaskTwoTests {
     @Autowired
     private FileLoader fileLoader;
 
+
     @Test
     void task_two_verifier() throws InterruptedException {
         String[] transactionLines = fileLoader.loadStrings("/test_data/poiuytrewq.uiop");
