d6746129383e2fec3c604173e7322b4a54c0dcf9
Divyansh Kachangal <dkachangal@gmail.com>
2025-11-26 22:28:30 +0530

    slight update

--- src/test/java/com/jpmc/midascore/ListenKafka.java
+++ src/test/java/com/jpmc/midascore/ListenKafka.java
@@ -1,8 +1,14 @@
 package com.jpmc.midascore;
 
+import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ListenKafka {
-    @KafkaListener
-    
+
+    @KafkaListener(topics = "${general.kafka-topic}")
+    public void getMessage(Transaction transaction) {
+
+    }
 }
